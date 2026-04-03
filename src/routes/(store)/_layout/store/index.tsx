4b7ea354c7cc1cee3fdfda2a114253774377e2b3
Golam Mahabub Redoy <hasanhridoymahabub9@gmail.com>
2026-04-03 22:49:11 +0600

    feat: implement store listing and filtering functionality with UI components

--- src/routes/(store)/_layout/store/index.tsx
+++ src/routes/(store)/_layout/store/index.tsx
@@ -1,9 +1,10 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { StoreListingTemplate } from "#/components/templates/store/store-listing-template";
 
-export const Route = createFileRoute('/(store)/_layout/store/')({
+export const Route = createFileRoute("/(store)/_layout/store/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/(store)/_layout/store/"!</div>
+  return <StoreListingTemplate />;
 }
